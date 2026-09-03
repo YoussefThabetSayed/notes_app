4c63126e6974eb604380d6570bae2a82e1e89c30
Youssef Thabet Sayed <youssefthabetsayed@gmail.com>
2026-09-04 01:26:55 +0300

    add floating action buttom

--- lib/views/widgets/notes_list_view.dart
+++ lib/views/widgets/notes_list_view.dart
@@ -1,16 +1,17 @@
-
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_note_item.dart';
 
 class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
-
+  final data = const [
+    //in this var make multi colors to the note item
+  ];
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (context, item) {
         return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          padding: const EdgeInsets.symmetric(vertical: 4.0),
           child: NoteItem(),
         );
       },
