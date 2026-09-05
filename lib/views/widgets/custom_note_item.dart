6cfdb44eeef2f4a758ad1c467c2a6e85175c0901
Youssef Thabet Sayed <youssefthabetsayed@gmail.com>
2026-09-06 00:05:12 +0300

    create edit note view

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,51 +1,63 @@
-
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              'Flutter Tips',
-              style: TextStyle(fontSize: 26, color: Colors.black),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                'Flutter Tips',
+                style: TextStyle(fontSize: 26, color: Colors.black),
+              ),
+              subtitle: Padding(
+                padding: EdgeInsetsGeometry.only(top: 16, bottom: 16),
+                child: Text(
+                  'Bulid your career with youssef thabet',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.delete, color: Colors.black, size: 26),
+              ),
             ),
-            subtitle: Padding(
-              padding: EdgeInsetsGeometry.only(top: 16, bottom: 16),
+            Padding(
+              padding: EdgeInsetsGeometry.only(right: 24),
               child: Text(
-                'Bulid your career with youssef thabet',
+                'May 21,2022',
                 style: TextStyle(
+                  color: Colors.black.withOpacity(0.4),
                   fontSize: 16,
-                  color: Colors.black.withOpacity(0.5),
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.delete, color: Colors.black, size: 26),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsetsGeometry.only(right: 24),
-            child: Text(
-              'May 21,2022',
-              style: TextStyle(
-                color: Colors.black.withOpacity(0.4),
-                fontSize: 16,
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
