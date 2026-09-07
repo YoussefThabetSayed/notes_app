fe232e1143c389d3701cb82781bda2ba941a7ed8
Youssef Thabet Sayed <youssefthabetsayed@gmail.com>
2026-09-07 16:08:43 +0300

    edit color of float action button

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -9,6 +9,7 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
         onPressed: () {
           showModalBottomSheet(
             shape: RoundedRectangleBorder(
